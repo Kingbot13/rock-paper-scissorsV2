8f6179389dbd776a03cc25e42fdc2856ee2a0561
Dylan King <dwkblue51@yahoo.com>
2021-06-10 09:28:50 -0500

    add variables to keep score until 1 player reaches 5 points

--- index.js
+++ index.js
@@ -1,34 +1,38 @@
 function game(){
-
+    
     let playerScore = 0; // store scores
     let computerScore = 0;
-
+    
     const rock = document.querySelector("#rock");
     const paper = document.querySelector("#paper");
     const scissors = document.querySelector("#scissors");
+    
+    const gameResults = document.querySelector("div"); // div container for diplaying results
+    const scoreKeeper = document.createElement("p"); // add win, lose, or tie message to gameResults
+    const roundResults = document.createElement("p"); // add win, lose, or tie message to current round
 
 
-function playRound(playerSelection, computerSelection){
-    function computerPlay(){
+    function playRound(playerSelection, computerSelection){
+        function computerPlay(){
         const optionsArray = ["rock", "paper", "scissors"];
         let randomSelection = Math.floor(Math.random() * optionsArray.length);
         computerSelection = optionsArray[randomSelection];
         return computerSelection;
-    };
+        };
         computerPlay();
-        // playerSelection = document.querySelector(`button[id="${}"]`);
+        // playerSelection = button.id;
         const winning = `congrats! You win this round! ${playerSelection} beats ${computerSelection}`; // win, lose, and tie messages
         const losing = `Aww, you lost this round! ${computerSelection} beat ${playerSelection}`;
         const tie = `It's a tie! ${playerSelection} = ${computerSelection}`;
         
-        console.log("Player: ", playerSelection.toUpperCase());
+        console.log("Player: ", playerSelection);
         console.log("Computer: ", computerSelection.toUpperCase());
         
-        if (playerSelection.textContent.toLowerCase() === "rock"){ // assign values to choices to compare
+        if (playerSelection === rock){ // assign values to choices to compare
             playerSelection = 3;
-        } else if (playerSelection.textContent.toLowerCase() === "scissors"){
+        } else if (playerSelection === scissors){
             playerSelection = 2;
-        } else if (playerSelection.textContent.toLowerCase() === "paper"){ // value of 'paper' changes depending on computer's selection
+        } else if (playerSelection === paper){ // value of 'paper' changes depending on computer's selection
             if (computerSelection === "rock"){
                 playerSelection = 4;
             } else {
@@ -48,54 +52,45 @@ function playRound(playerSelection, computerSelection){
             }
         };
         
-        let results;
         
         if (playerSelection > computerSelection){ // update scores
-            results = winning;
+            roundResults.textContent = winning;
             ++playerScore;
             
             
         } else if(playerSelection < computerSelection){
-            results = losing;
+            roundResults.textContent = losing;
             ++computerScore;  
             
         } else {
-            results = tie;
+            roundResults.textContent = tie;
         };
         
-        let scoreReport = `Player Score: ${playerScore}  Computer Score: ${computerScore}`; // display scores
+        let scoreReport = document.createElement("p");
+
+        scoreReport.textContent = `Player Score: ${playerScore}  Computer Score: ${computerScore}`; // display scores
         
-        console.log(results);
         console.log(scoreReport);
 
+        gameResults.appendChild(roundResults);
     };
 
     const buttons = document.querySelectorAll("button");
+    
+    buttons.forEach((button) => { // add event listeners to buttons
+        button.addEventListener("click", playRound);
+    });
+    
+    
 
-    buttons.forEach(addEventListener("click", function(e){
-        playerSelection = e.button;
-        playRound();
-    }));
-
-    const gameResults = document.querySelector("div"); // div container for diplaying results
-
-    const scoreKeeper = document.createElement("p")
-
-    if (playerScore > computerScore){ // announce win, lose, or tie message for playing all 5 games
+    if (playerScore === 5){ // announce win, lose, or tie message for playing all 5 games
         scoreKeeper.textContent = "You are the ultimate champion!";
-    } else if (playerScore < computerScore) {
+    } else if (computerScore === 5) {
         scoreKeeper.textContent = "You lose the game!";
-    } else {
-        scoreKeeper.textContent = "Wow! You tied!";
     };
-
+    
     gameResults.appendChild(scoreKeeper);
-
-
-
-
+    
 };
 
 game();
-
-
